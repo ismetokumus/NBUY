5714a158d60e09a18280e465610cd4cb3de04165
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-10-03 18:10:35 +0300

    03.10.2022 son

---
+++ Proje01_Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Proje01_Hello_World
+{
+    internal class Program//Burası programın ana scopunun başlangıç noktası
+    {
+        static void Main(string[] args)//Burasıda ilk çalışacak olan metod!!!
+        {
+            Console.WriteLine("Merhaba, Dünya!");
+            Console.WriteLine("Merhaba, Dünya!");
+        }
+    }
+}
