b1685aa74ba3db1322805dcced2fadf15f6f20d3
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-10-06 18:20:16 +0300

    06.10.2022

--- Week_01/Proje11_For/Program.cs
+++ Week_01/Proje11_For/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Proje11_For
+﻿using System;
+
+namespace Proje11_For
 {
     internal class Program
     {
@@ -41,7 +43,7 @@
             Console.WriteLine(toplam);
             */
             //1-10 sayıların çift ve tek sayı top
-
+            /*
             int toplamC = 0;
             int toplamT = 0;
             for (int i = 0; i <= 10; i++)
@@ -57,6 +59,138 @@
             }
             Console.WriteLine($"Çift sayıların toplamı: {toplamC}");
             Console.WriteLine($"Tek sayıların toplamı: {toplamT}");
+            */
+
+            //SORU Klavyeden 2 sayı girilsin
+            //bu sayıların arasındaki sayıların toplamını ekrana yazdırın
+            /*
+            Console.Write("1. Sayıyı giriniz: ");
+            int sayi1=Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("2. Sayıyı giriniz: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            for (int i = sayi1; i <=sayi2; i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine($"Toplam: {toplam}");
+            */
+            /*
+            Console.Write("1. Sayıyı giriniz: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. Sayıyı giriniz: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            if (sayi1 < sayi2)
+            {
+                for (int i = sayi1; i <= sayi2; i++)
+                {
+                    toplam += i;
+                }
+            }
+            else
+            {
+                for (int i = sayi2; i <= sayi1; i++)
+                {
+                    toplam += i;
+                }
+
+            }
+            Console.WriteLine($"Toplam: {toplam}");
+            */
+            /*
+            Console.Write("1. Sayıyı giriniz: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. Sayıyı giriniz: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            int min=Math.Min(sayi1,sayi1);
+            int max=Math.Max(sayi1,sayi2);
+            for (int i = min; i <= max; i++)
+            {
+                toplam += i;
+            }
+            Console.WriteLine($"Toplam: {toplam}");
+            */
+
+            //SORU=> Program kullanıcıdan bir sayı girmesini istesin. Ancak kullanıcı sayı girmeye devam etikçe girilen sayıların toplanmasını sağlayalım. Sayı adedi belirsizdir. Uygulamanın bitip, ekrana toplamı yazdırabilmesi için kullanıcı sayı yerine exit yazması lazım.
+            /*
+            int toplam = 0;
+            string girilenDeger = "";
+            string sonucMetin = "";
+            string sonEk = " + ";
+            for (int i = 1; i < 100000; i++)
+            {
+                Console.Write($" {i}. sayıyı giriniz(Çıkış için exit)[Geçerli toplam: {toplam}]: ");
+                girilenDeger=Console.ReadLine();
+                if (girilenDeger.ToLower()=="exit")//if küçük büyük harf duyarlıdır.
+                {
+                    break;//içinde bulunan döngünün tamamlanmasını beklemeden,istenilen bir anda durdurulması için kullanılır
+                }
+                sonucMetin = sonucMetin + girilenDeger + sonEk;
+                toplam +=Convert.ToInt32(girilenDeger);
+                
+            }
+            int alinacakKisminUzunlugu = sonucMetin.Length - sonEk.Length;
+            sonucMetin=sonucMetin.Substring(0, alinacakKisminUzunlugu); 
+            Console.WriteLine($"{sonucMetin}={toplam}");
+            */
+            /*
+            //SORU 5*5 lik bir kare biçimini yıldız işaretleri oluşturan uygulama.
+
+            
+            int satir = 5;
+            int sutun = 5;
+            for (int i = 0; i < satir; i++)
+            {
+                //a
+                for (int j = 0; j < sutun; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();    
+            }
+            */
+            /*
+            //SORU 5*5 lik bir kare içi boş biçimini yıldız işaretleri oluşturan uygulama.
+            
+
+            int satir = 5;
+            int sutun = 5;
+            for (int i = 1; i <= satir; i++)
+            {
+                //a
+                for (int j = 1; j <= sutun; j++)
+                {
+
+                    if (i==1 || i==satir)//eğer 1. veya son satırdaysam
+                    {
+                        Console.Write("*");
+                    }
+                    else if (j==1 || j==sutun)//eğer 1. veya son stündaysan
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            */
+            //ÖDEV Üsttetik içi boş kareyi oluşturan uygulamayı ADIM ADIM çalıştırarak anlamaya çalışın
+            //ÖDEV-1 : 1-100 ARASINDAKİ SAYILARIN ORTALAMASINI BULDURAN PROGRAM
+            //ÖDEV-2 : 1-100 ARASINDAKİ ÇİFT,TEK VE 5 İN KATI OLAN SAYILARIN ADETLERİ VE TOPLAMLARINI EKRANA YAZDIRAN PROGRAM
+            //ÖDEV-3 : AŞAĞIDAKİ YÜKSEKLİĞİ 5, TABANI 9 BİRİM OLAN YILDIZLARDAN OLUŞAN DİK ÜÇGEGİ ÇİZDİREN PROGRAM
+            /*
+             * *
+             * ***
+             * *****
+             * *******
+             * *********
+             */
         }
     }
 }
---
+++ Week_01/Proje12_While/Program.cs
@@ -0,0 +1,159 @@
+﻿namespace Proje12_While;
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        //while belirli kural devam ettikçe sonsuz döngüde kullanılır.
+        int sayac=1;
+        while(sayac<=6)//kışul sağlanıyorsa sürekli döngü içerisinde.
+        {
+            System.Console.WriteLine("Merhaba !");
+            sayac ++;
+        }
+        */
+        /*
+        int toplam=0;
+        string girilenDeger="";
+        int sayac=1;
+        while (girilenDeger!="exit")
+        {
+            System.Console.Write($"{sayac}. sayıyı giriniz(Çıkış için exit): ");
+            girilenDeger=Console.ReadLine();
+            //if(girilenDeger!="exit") toplam+=Convert.ToInt32(girilenDeger);
+            try//hata varsa kullan!!!
+            {
+                toplam+=Convert.ToInt32(girilenDeger);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine(toplam);
+                break;
+            }
+            sayac++;
+        }
+        */
+        //Klavyeden exit yazılan kadar isim girilmesini isteyen uygulama.
+        //çözüm 1
+        /*
+        string isim="";
+        while (isim!="exit")
+        {
+            System.Console.Write("İsim giriniz: ");
+            isim=Console.ReadLine();
+        }
+        System.Console.WriteLine("Program sona erdi...");
+        */
+        //çözüm 2
+        /*string isim;
+        do
+        {
+            System.Console.Write("İsim girinzi: "); 
+            isim=Console.ReadLine();
+        } while (isim!="exit");
+        Console.WriteLine("Program sona erdi...");
+        */
+
+        //Klavyeden exit yazılana kadar 
+        /*
+        string girilenDeger;
+        int sayac=1;
+        int toplam=0;
+        do
+        {
+            System.Console.Write($"{sayac}.sayıyı giriniz: ");
+            girilenDeger = Console.ReadLine();
+            if (girilenDeger!="exit") toplam+=Convert.ToInt32(girilenDeger);
+            sayac++;
+        } while (girilenDeger!="exit");
+        */
+        /*
+        //rastgele değer üretme
+
+        Random rastgele=new Random();
+        int rastgeleSayi=rastgele.Next();
+        System.Console.WriteLine(rastgeleSayi);
+        int rastGele2=rastgele.Next(100);//100 dahil değildir. 0 daildir
+        System.Console.WriteLine(rastGele2);
+        int rastGele3=rastgele.Next(100,200);//100 dahil ve 200 arasın dahil değildir.
+        System.Console.WriteLine(rastGele3);
+        */
+
+        //OYOUN: Uygulamamnın rastgele uyduracağı bir sayının kullanıcının tahmin etmesini isteyeceğiz.
+        //Rastgele üretilecek sayı 1-11 arasında olsun.
+        //Kullanıcı rastgele sayıdan küçük yada büyük sayı girdiğinde kullanıcıya düzgün
+        //bir şekilde mesaj verilsin.
+        //kullanıcı doğru sayıyı tahmin ettiyse ya da 5 hakkını kullandıysa uygulama sona ersin
+
+
+        Random rnd = new Random();
+        int uretilenSayi = rnd.Next(1, 101);
+        System.Console.WriteLine($"HİLE: {uretilenSayi}");
+        System.Console.WriteLine("*********************");
+        int tahminEdilenSayi;
+        int hak = 1;//kullanıcının o sırada kaçıncı hakkını kullandığı bilgisi
+        int haksiniri = 5;//kullanıcının kaç hakkı kaldığı
+        // do
+        // {
+
+        //     Console.Write($"{hak}. Tahmininizi giriniz(1-100): ");
+        //     tahminEdilenSayi = Convert.ToInt32(Console.ReadLine());
+
+        //     if (hak == haksiniri && uretilenSayi != tahminEdilenSayi)
+        //     {
+        //         System.Console.WriteLine("KAYBETTİNİZ");
+        //         break;
+        //     }
+
+        //     if (tahminEdilenSayi > uretilenSayi)
+        //     {
+        //         System.Console.WriteLine("Büyük bir değer girdiniz , daha küçük bir değer giriniz!");
+        //     }
+        //     else if (tahminEdilenSayi < uretilenSayi)
+        //     {
+        //         System.Console.WriteLine("Küçük bir değer girdiniz , daha büyük bir değer giriniz!");
+        //     }
+        //     else
+        //     {
+        //         System.Console.WriteLine("Tebrikler,bildiniz");
+        //     }
+        //     hak++;
+
+
+
+        // } while (tahminEdilenSayi < uretilenSayi && hak <= haksiniri);
+
+        do
+        {
+            Console.Write($"{hak}. Tahmininizi giriniz(1-100): ");
+            tahminEdilenSayi = Convert.ToInt32(Console.ReadLine());
+            if (tahminEdilenSayi>uretilenSayi)
+            {
+                System.Console.WriteLine("Büyük girdin");
+            }
+            else if(tahminEdilenSayi<uretilenSayi)
+            {
+                System.Console.WriteLine("Küçük girdin");
+            }
+            if(tahminEdilenSayi!=uretilenSayi)
+            {
+                hak++;
+                if (hak<=haksiniri)
+                {
+                    System.Console.WriteLine("Kaybettin");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Kazandın!");
+            }
+            
+        } while (tahminEdilenSayi != uretilenSayi && hak<=haksiniri );
+        //eğer program bu satıra gelmişse ya doğru tahminde bulunulmuştur ya da hak sona ermiştir.
+
+
+
+
+
+    }
+}
