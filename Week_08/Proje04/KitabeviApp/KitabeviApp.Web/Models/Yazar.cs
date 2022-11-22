cad390e44af1409fcf12037a9cb89a583c0f7d24
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-11-22 18:22:50 +0300

    22.11.2022 Mimari giriş yapıldı

---
+++ Week_08/Proje04/KitabeviApp/KitabeviApp.Web/Models/Yazar.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace KitabeviApp.Web.Models
+{
+    public class Yazar
+    {
+        public int Id { get; set; }
+
+        public string Ad { get; set; }
+
+        public int? DogumYili { get; set; }
+        public char Cinsiyet { get; set; }
+    }
+}
