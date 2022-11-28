0baf69626c7ccb7c7e08a06e78b044a78bcf653f
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-11-28 18:06:47 +0300

    28.11.2022

---
+++ Week_09/28-11-2022/BlogApp/BlogApp.Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using BlogApp.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Entities.Concrete
+{
+    public class Category : EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+        //Lİst, ArrayList, Array, IList, ICollection yapılarını miras alma özellikleri
+        //Açısından inceleyiniz. (Microsoft dökümantasyonu)
+    }
+}
