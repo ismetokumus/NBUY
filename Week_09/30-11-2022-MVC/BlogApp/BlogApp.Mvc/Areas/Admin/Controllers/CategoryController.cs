57e7ffa6c0e12830798ef26f4a84f2daaacdba83
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-11-30 18:11:56 +0300

    30.11.2022

---
+++ Week_09/30-11-2022-MVC/BlogApp/BlogApp.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BlogApp.Services.Abstract;
+using BlogApp.Shared.Utilities.Result.ComplexTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Mvc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+
+        private readonly ICategoryService _categoryService;
+        private readonly IArticleService _articleService;
+
+        public CategoryController(ICategoryService categoryService, IArticleService articleService)
+        {
+            _categoryService = categoryService;
+            _articleService = articleService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result=await _categoryService.GetAll();
+            if (result.ResultStatus==ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+            return View();
+        }
+    }
+}
