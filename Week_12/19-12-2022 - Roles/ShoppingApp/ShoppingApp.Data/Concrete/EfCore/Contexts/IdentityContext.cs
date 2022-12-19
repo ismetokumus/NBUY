8f2cde33c34933b537c5749fe077cec7d2d6a9d3
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-12-19 18:02:32 +0300

    19.12.2022 eksik var

---
+++ Week_12/19-12-2022 - Roles/ShoppingApp/ShoppingApp.Data/Concrete/EfCore/Contexts/IdentityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Entity.Concrete.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.Data.Concrete.EfCore.Contexts
+{
+    public class IdentityContext : IdentityDbContext<User,Role,string>//string primary keyin tipleri için yazıldı.
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+    }
+}
