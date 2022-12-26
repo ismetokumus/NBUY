fb568fad65ff104a8c045664b947e328ee3365fa
ismetokumus <45925216+ismetokumus@users.noreply.github.com>
2022-12-26 18:17:47 +0300

    26.12.2022

---
+++ Week_13/26-12-2022-my/ShoppingApp/ShoppingApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Business.Abstract;
+using ShoppingApp.Business.Concrete;
+using ShoppingApp.Entity.Concrete;
+using ShoppingApp.Web.Areas.Admin.Models.Dtos;
+using ShoppingApp.Web.Models.Dtos;
+
+namespace ShoppingApp.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    [Area("Admin")]
+    
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productManager;
+
+        public HomeController(IProductService productManager)
+        {
+            _productManager = productManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SearchAdmin(string bul)
+        {
+            List<Product> searchResults = await _productManager.GetSearchResultsAsync(bul);
+            List<ProductListDto> productDtos = new List<ProductListDto>();
+            foreach (var product in searchResults)
+            {
+                productDtos.Add(new ProductListDto
+                {
+                    Product =product
+                });
+            }
+            ViewBag.Title = "Ürün Arama Sonuçları";
+            return View(productDtos);
+
+        }
+    }
+   
+}
---
+++ Week_13/26-12-2022-my/ShoppingApp/ShoppingApp.Web/Extensions/MigrationManager.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Data.Concrete.EfCore.Contexts;
+
+namespace ShoppingApp.Web.Extensions
+{
+    public static class MigrationManager//Extensions metodları static olmalı
+    {
+        public static IHost UpdateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                using (var shopAppContext = scope.ServiceProvider.GetRequiredService<ShopAppContext>())
+                {
+                    try
+                    {
+                        shopAppContext.Database.Migrate();//Bu komut enüz işlenmemiş migrationları alır ve database update işlemine tabi tutar.
+                    }
+                    catch (Exception)
+                    {
+                        //Hataların tutulduğu loglar burada hazırlanıyor.
+                        throw;
+                    }
+
+                }
+            }
+            return host;
+        }
+    }
+}
